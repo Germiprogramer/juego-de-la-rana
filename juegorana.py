73a808d7b7638cab9849e724184e4fddb9e50504
claudiaalozano <clau.lozano.perez@gmail.com>
2021-12-01 19:05:01 +0100

    commit 1

--- juegorana.py
+++ juegorana.py
@@ -3,3 +3,33 @@ import os
 import random
 import re
 import sys
+
+if __name__ == "__main__": 
+    first_multiple_input= input().rstrip().split()
+
+
+    n=int(first_multiple_input[0])
+
+    m= int(first_multiple_input[1])
+
+    k=int(first_multiple_input[2])
+
+    for n_itr in range(n):
+        row = input()
+
+        #escribir nuestro código
+
+    for k_itr in range(k):
+        second_multiple_input = input().rstrip().split()
+
+        i1 = int(second_multiple_input[0])
+
+        j1 = int(second_multiple_input[1])
+
+        i2 = int(second_multiple_input[2])
+
+        j2 = int(second_multiple_input[3])
+
+        #escribir nuestro código
+
+        
