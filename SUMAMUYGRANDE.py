9c23bc7b94f54ed9182f56330229b34096f1512f
Germiprogramer <llorentemunozgerman@gmail.com>
2021-12-11 18:07:48 +0100

    commit

---
+++ SUMAMUYGRANDE.py
@@ -0,0 +1,10 @@
+matriz = [
+    [1, 2],
+    [3, 4],
+]
+
+def suma(matriz):
+    sumamatriz = (matriz[0])[0] + (matriz[1])[0] + (matriz[0])[0] + (matriz[0])[1]
+    print(sumamatriz)
+    
+suma(matriz)
--- h = 1.py
+++ h = 1.py
@@ -1,10 +1,2 @@
-import numpy as np
 
-BL = (255, 255, 255)
-NG = (0, 0, 0)
-
-tablero = [
-    [BL, "NG", "BL"]
-]
-print(tablero)
 
