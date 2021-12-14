defbe08e65a74b2066d612a905a27d601cd78c39
Germiprogramer <llorentemunozgerman@gmail.com>
2021-12-14 19:55:34 +0100

    commit

--- SUMAMUYGRANDE.py
+++ SUMAMUYGRANDE.py
@@ -1,7 +1,15 @@
+from random import randint
+import numpy as np
+np.array([1,2],[3,4])
+
+print(np.array([1,2],[3,4]))
+
 matriz = [
     [1, 2],
     [3, 4],
 ]
+
+
 
 def suma(matriz):
     sumamatriz = (matriz[0])[0] + (matriz[1])[0] + (matriz[0])[0] + (matriz[0])[1]
