47bf415fa3d701fd113f7d71358677b59e6c701b
Germiprogramer <llorentemunozgerman@gmail.com>
2021-12-18 13:59:43 +0100

    commit

--- compara los problemas ARCHIVO PRUEBA.py
+++ compara los problemas ARCHIVO PRUEBA.py
@@ -1,22 +1,15 @@
 from math import atan2
 from random import randint
 
-a1 = randint(0,10)
-a2 = randint(0,10)
-a3 = randint(0,10)
-b1 = randint(0,10)
-b2 = randint(0,10)
+a1 = randint(0,100)
+a2 = randint(0,100)
+a3 = randint(0,100)
+b1 = randint(0,100)
+b2 = randint(0,100)
 b3 = randint(0,10)
 
 print("Las notas de Lucía son>>\nCLARIDAD DEL PROBLEMA = {} \nORIGINALIDAD = {} \nDIFICULTAD = {}".format(a1,a2,a3))
 print("Las notas de Carlos son>>\nCLARIDAD DEL PROBLEMA = {} \nORIGINALIDAD = {} \nDIFICULTAD = {}".format(b1,b2,b3))
-
-#a1 = input("SELECCIONA LA PRIMERA NOTA (claridad del problema) DE LUCÍA>>>")
-#a2 = input("SELECCIONA LA SEGUNDA NOTA (originalidad) DE LUCÍA>>>")
-#a3 = input("SELECCIONA LA TERCERA NOTA (dificultad) DE LUCÍA>>>")
-#b1 = input("SELECCIONA LA PRIMERA NOTA (claridad del problema) DE CARLOS>>>")
-#b2 = input("SELECCIONA LA SEGUNDA NOTA (originalidad) DE CARLOS>>>")
-#b3 = input("SELECCIONA LA TERCERA NOTA (dificultad) DE CARLOS>>>")
 
 
 a = [a1, a2, a3]
